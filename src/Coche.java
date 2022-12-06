e24ad7b80149b25b9fa7617df574d55133f21d35
legiónDev <ernetrades@gmail.com>
2022-12-06 07:11:05 -0500

    test a punto de enviar

--- src/Coche.java
+++ src/Coche.java
@@ -1,16 +1,27 @@
+import javax.print.event.PrintEvent;
 
 public class Coche{
     //Atributos
-    String color;
-    int km;
+    /*String color;
+    int km;*/
+    int puertas;
     
     //Metodos
     public static void main(String[] args) {
         //Objeto
         Coche coche1 = new Coche();
-        coche1.color = "blanco";
-        coche1.km = 65;
+        /*coche1.color = "blanco";
+        coche1.km = 65;*/
+        coche1.puertas = 4;
 
-        System.out.println("El color del coche es: " + coche1.color );
+        //System.out.println("El color del coche es: " + coche1.color );
+       int p = suma(1) + coche1.puertas;
+       System.out.println("El coche tiene " + p + " puertas") ;
     }
+
+    //Funcion
+    public static int suma(int a){
+        
+        return a;
+     }
 }
